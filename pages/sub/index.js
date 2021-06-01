0fd5a31d4a953ce9f56eb66562f97bc5ef7e40d4
kelvin9314 <kelvin9314@gmail.com>
2021-06-01 14:47:20 +0800

    Fetching Data (getStaticProps)

--- pages/sub/index.js
+++ pages/sub/index.js
@@ -1,9 +1,33 @@
-const Sub = (params) => {
+import styles from '../../styles/Sub.module.css'
+
+// NOTE run in build time
+export const getStaticProps = async ( context ) => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+  const data = await res.json()
+
+
+  return{
+    props: {
+      subs: data
+    }
+  }
+}
+
+const Sub = ({subs}) => {
   return (
     <div>
       <h1>
         All subs
       </h1>
+      
+      {subs.map(sub => (
+        <div key={sub.id}>
+          <a className={styles.single}>
+            <h3>{sub.name}</h3>
+          </a>
+        </div>
+      ))}
+    
     </div>
   )
 }
